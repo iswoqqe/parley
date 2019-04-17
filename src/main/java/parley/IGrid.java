b854beda2873a42ca7edd93327f01ef089edb864
Axel Bergström <axel.bergstrom00@gmail.com>
2019-04-17 13:51:24 +0200

    change bool to boolean

--- src/main/java/parley/IGrid.java
+++ src/main/java/parley/IGrid.java
@@ -5,5 +5,5 @@ import java.util.List;
 public interface IGrid {
     List<GameObject> get(int x, int y);
     void add(int x, int y, GameObject object);
-    bool isBlocked(int x, int y);
+    boolean isBlocked(int x, int y);
 }
